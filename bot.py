e907f651ef41dbd9adeb09bdfb69a8985f37cc2f
khaldonart1 <khaldonatef@gmail.com>
2025-06-12 22:16:43 +0300

    final

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ logger = logging.getLogger(__name__)
 # --- Configuration ---
 class Config:
     """Manages all static configuration for the bot for easy access and modification."""
-    BOT_TOKEN = "7950170561:AAECeQpxb1G4zrnFhrol_uBgPNoxZN-Qkz0"
+    BOT_TOKEN = "7950170561:AAFIrmAM5zzHQlvuUpR8KQeTSjRZ1_2Mi8M"
     SUPABASE_URL = "https://jofxsqsgarvzolgphqjg.supabase.co"
     SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZnhzcXNnYXJ2em9sZ3BocWpnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTU5NTI4NiwiZXhwIjoyMDY1MTcxMjg2fQ.egB9qticc7ABgo6vmpsrPi3cOHooQmL5uQOKI4Jytqg"
 
--- check_env.py
+++ check_env.py
@@ -2,7 +2,7 @@ import os
 
 print("--- Starting Environment Variable Check ---")
 
-bot_token = os.environ.get("7950170561:AAER-L3TyzKll--bl4n7FyPVxLxsFju6wSs")
+bot_token = os.environ.get("7950170561:AAFIrmAM5zzHQlvuUpR8KQeTSjRZ1_2Mi8M")
 supabase_url = os.environ.get("https://jofxsqsgarvzolgphqjg.supabase.co")
 supabase_key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZnhzcXNnYXJ2em9sZ3BocWpnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTU5NTI4NiwiZXhwIjoyMDY1MTcxMjg2fQ.egB9qticc7ABgo6vmpsrPi3cOHooQmL5uQOKI4Jytqg")
 
